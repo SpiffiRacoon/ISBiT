d2a0e96c6a7bdc3655d753d993bd2e3e97d9f4cd
LinusHakegard <linus.hakegard@gmail.com>
2024-09-29 18:40:00 +0200

    basic navbar

--- Frontend/isbit/src/main.ts
+++ Frontend/isbit/src/main.ts
@@ -4,6 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Import Bootstrap JavaScript (optional if you're using JS components)
+import 'bootstrap'
+
 const app = createApp(App)
 
 app.use(router)

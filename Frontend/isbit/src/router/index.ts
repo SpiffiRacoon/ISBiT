23bdf9a7d6b471e0a300da414f02b0d10ae06811
Sinoeng <simonspost2012@gmail.com>
2024-10-01 16:09:05 +0200

    added label view

--- Frontend/isbit/src/router/index.ts
+++ Frontend/isbit/src/router/index.ts
@@ -21,6 +21,11 @@ const router = createRouter({
       path: '/contact',
       name: 'contact',
       component: () => import('../views/ContactView.vue')
+    },
+    {
+      path: '/label',
+      name: 'label',
+      component: () => import('../views/LabelView.vue')
     }
   ]
 })

c5b89c06969b78de01dd4bd0ccbec94119b4b820
Leo <leohemmingsson@gmail.com>
2024-10-14 15:37:47 +0200

    Added id on nodes

--- Backend/src/types/node.py
+++ Backend/src/types/node.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Node(BaseModel):
@@ -6,12 +6,23 @@ class Node(BaseModel):
     Datamodel for a node
     """
 
-    _id: int | None = None
+    id: str | None = None
     cluster: int
     text: str
     x: float
     y: float
     truth: str
 
-    def __init__(self, **data):
-        super().__init__(**data)
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                    {"id": "123", "cluster": 1, "text": "This is a text", "x": 0.1, "y": 0.2, "truth": "true"},
+            ]
+        }
+    }
+
+    def __init__(self, *args, **kwargs):
+
+        if "_id" in kwargs:
+            kwargs["id"] = str(kwargs["_id"])
+        super().__init__(*args, **kwargs)

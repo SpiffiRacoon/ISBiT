66ca22a184cd3b1f29216b320655b33dcf3262e0
LinusHakegard <linus.hakegard@gmail.com>
2024-10-21 18:14:20 +0200

    save

--- Frontend/isbit/src/components/label_menu/LabelBox.vue
+++ Frontend/isbit/src/components/label_menu/LabelBox.vue
@@ -54,7 +54,7 @@ export default defineComponent({
     display: flex;
     flex-direction: column;
     align-items: center;
-    background-color: blueviolet;
+    background-color: white;
 }
 
 </style>

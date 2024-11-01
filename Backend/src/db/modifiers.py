667d1bd701dc9283891d00ab4d4c34aa5260d3a8
Leo Hemmingsson <70817316+Leohemmingsson@users.noreply.github.com>
2024-11-01 11:19:59 +0100

    Async runs works (#59)

--- Backend/src/db/modifiers.py
+++ Backend/src/db/modifiers.py
@@ -10,3 +10,12 @@ def delete_collection(collection: str, ConnectionClass=MongoConnection) -> None:
     with ConnectionClass() as (_, db):
         db_col = db[collection]
         db_col.drop()
+
+
+def delete_ml_status(id: str, ConnectionClass=MongoConnection) -> None:
+    """
+    Delete a ml_status entry
+    """
+    with ConnectionClass() as (_, db):
+        query = {"ml_id": id}
+        db["ml_status"].delete_many(query)

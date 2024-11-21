63c9efae5eb3be45bb1f99b39c6b5c40de7e53fc
LinusHakegard <linus.hakegard@gmail.com>
2024-11-21 15:00:34 +0100

    merge

---
+++ Frontend/isbit/src/components/label_menu/MultiplePointText.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="label-text">
+      <div v-for="(point, index) in points" :key="index" class="point-item">
+        <p>{{ point.text }}</p> 
+      </div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, type PropType } from 'vue'
+
+  import type { CustomPoint } from './ClusterPlot.vue'
+  
+
+  export default defineComponent({
+    props: {
+      points: {
+        type: Array as PropType<CustomPoint[]>,
+        required: true
+      }
+    }
+  })
+  </script>
+  
+  <style scoped>
+  #label-text {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+  
+  .point-item {
+    padding: 5px 10px;
+    border: 1px solid #ccc; 
+    border-radius: 4px;
+    background-color: #f9f9f9; 
+  }
+  
+  .point-item p {
+    margin: 0;
+    font-size: 14px;
+  }
+  </style>
+  
---
+++ Frontend/isbit/src/components/label_menu/TextBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="label-text">
+    <p class="point-item">
+      {{ text }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, type PropType } from 'vue'
+export default defineComponent({
+  props: {
+    text: String
+  }
+})
+</script>
+
+<style scoped>
+  #label-text {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+  
+  .point-item {
+    padding: 5px 10px;
+    border: 1px solid #ccc; 
+    border-radius: 4px;
+    background-color: #f9f9f9; 
+  }
+</style>

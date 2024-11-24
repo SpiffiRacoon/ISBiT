f0d2c117bb2e516563c65af5e414359d5c39a215
Leo <leohemmingsson@gmail.com>
2024-11-24 14:55:14 +0100

    Added routes to see dim and model, also makde models more dynamic

--- Backend/src/v1/__init__.py
+++ Backend/src/v1/__init__.py
@@ -3,7 +3,7 @@ from .data import router as data_router
 from .dataset import router as collections_router
 from .run_ml import router as run_ml_router
 from .testing import router as testing_router
+from .ml import router as ml_router
 
 
-
-__all__ = ["ping_router", "data_router", "collections_router", "run_ml_router", "testing_router"]
+__all__ = ["ping_router", "data_router", "collections_router", "run_ml_router", "testing_router", "ml_router"]
